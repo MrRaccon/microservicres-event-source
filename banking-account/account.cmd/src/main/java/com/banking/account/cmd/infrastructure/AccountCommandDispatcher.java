e24c973029a278e9fce70491568d6c6ab604b4f7
Alexis Biñuelo <31171057+MrRaccon@users.noreply.github.com>
2023-02-06 19:05:15 -0600

    se agrega patron mediador

---
+++ banking-account/account.cmd/src/main/java/com/banking/account/cmd/infrastructure/AccountCommandDispatcher.java
@@ -0,0 +1,41 @@
+package com.banking.account.cmd.infrastructure;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.banking.cqrs.core.commands.BaseCommand;
+import com.banking.cqrs.core.commands.CommandHabdlerMethod;
+import com.banking.cqrs.core.infraestructure.CommandDispatcher;
+
+@Service
+public class AccountCommandDispatcher implements CommandDispatcher {
+
+	private final Map<Class<? extends BaseCommand>, List<CommandHabdlerMethod>> routes = new HashMap<>();
+
+	@Override
+	public <T extends BaseCommand> void registerHandler(Class<T> type, CommandHabdlerMethod<T> handler) {
+		//AGREGA UN NUEVO VALOR 
+		var handlers = routes.computeIfAbsent(type, c -> new LinkedList<>());
+		handlers.add(handler);
+	}
+ 
+	@Override
+	public void send(BaseCommand command) {
+		// TODO Auto-generated method stub
+		//UN COMMAND SOLO PUEDE TENER UN HANDLER
+			var handlers = routes.get(command.getClass());
+			if(handlers ==null||handlers.isEmpty()) {
+				throw new RuntimeException("El command handler no fue registrado");
+			}
+			if(handlers.size()>1) {
+				throw new RuntimeException("No se puede enviar un command que tiene mas de un handler");
+			}
+
+			handlers.get(0).handle(command);
+	}
+
+}
